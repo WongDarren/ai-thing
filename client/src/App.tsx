196bc903cd10c590d7937d2777df437561bd36d6
Darren Wong <wongdarren1997@gmail.com>
2023-03-01 22:42:27 -0600

    Set up App.tsx with basic styles and routes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,41 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { logo } from "./assets";
+import { CreatePost, Home } from "./pages";
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header
+        className={
+          "flex w-full items-center justify-between border-b border-b-[#e6ebf4] bg-white px-4 py-4 sm:px-8"
+        }
+      >
+        <Link to={"/"}>
+          <img src={logo} alt={"logo"} className={"w-28 object-contain"} />
+        </Link>
+        <Link
+          to={"/create-post"}
+          className={
+            "font-inter rounded-md bg-[#6469ff] px-4 py-2 font-medium text-white"
+          }
+        >
+          Create
+        </Link>
+      </header>
+      <main
+        className={
+          "min-h-[calc(100vh - 73px)] w-full bg-[#f9fafe] px-4 py-8 sm:p-8"
+        }
+      >
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/create-post"} element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+};
 
 export default App;
