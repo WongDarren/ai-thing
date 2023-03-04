a4ef8713112edc8f8e9ee7d9c31438fc5aacae4b
Darren Wong <wongdarren1997@gmail.com>
2023-03-04 00:40:50 -0600

    Replaced localhost:8080 with hosted backend url.

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -15,11 +15,14 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ prompt: form.prompt }),
-        });
+        const response = await fetch(
+          "https://ai-thing.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ prompt: form.prompt }),
+          },
+        );
 
         const data = await response.json();
 
@@ -41,13 +44,16 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
+        const response = await fetch(
+          "https://ai-thing.onrender.com/api/v1/post",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
           },
-          body: JSON.stringify(form),
-        });
+        );
 
         await response.json();
         navigate("/");
